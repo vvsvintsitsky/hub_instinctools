12854e02f463888f28e4d97c7817bfd0a8b4df74
vvsvintsitsky <v.v.svintsitsky@gmail.com>
2017-02-06 16:19:40 +0300

    no message

--- src/main/java/wsvintsitsky/instinctools/jpa/dataaccess/impl/AbstractDaoImpl.java
+++ src/main/java/wsvintsitsky/instinctools/jpa/dataaccess/impl/AbstractDaoImpl.java
@@ -16,23 +16,32 @@ public class AbstractDaoImpl<T, ID> implements AbstractDao<T, ID> {
 	@Override
 	public T insert(T entity) {
 		EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+		entityManager.getTransaction( ).begin( );
 		entityManager.persist(entity);
+		entityManager.getTransaction( ).commit( );
+		entityManager.close( );
 		return entity;
 	}
 
 	@Override
 	public T update(T entity) {
 		EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+		entityManager.getTransaction( ).begin( );
 		entity = entityManager.merge(entity);
 		entityManager.flush();
+		entityManager.getTransaction( ).commit( );
+		entityManager.close( );
 		return entity;
 	}
 
 	@Override
 	public void delete(ID id) {
 		EntityManager entityManager = ENTITY_MANAGER_FACTORY.createEntityManager();
+		entityManager.getTransaction( ).begin( );
 		entityManager.createQuery(String.format("delete from %s e where e.id = :id", entityClass.getSimpleName()))
 				.setParameter("id", id).executeUpdate();
+		entityManager.getTransaction( ).commit( );
+		entityManager.close( );
 	}
 
 }
